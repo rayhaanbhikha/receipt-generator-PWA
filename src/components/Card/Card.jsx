79b87efcdb549d7cd9e408fcb5b2dd0f90c58c3f
Ray <rayhaan.bhikha@sky.uk>
2019-05-03 23:55:09 +0100

    dynamic card added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,16 +7,37 @@ import Colors from "../Blocks/Colors/Colors";
 import "./Card.css";
 
 export default class Card extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            hidden: true
+        }
+    }
+
+    toggleHidden = () => {
+        this.setState(prevState => {
+            return {
+                hidden: !prevState.hidden
+            }
+        })
+    }
+
     render() {
         return (
             <div className="card-wrapper">
+                <div className="card-wrapper2">
 
-                <div className="card">
-                    <Prices />
-                    <hr className="hr" />
-                    <ClothingTypes />
-                    <hr className="hr" />
-                    <Colors />
+                    <div className="card-toggle" onClick={this.toggleHidden}>
+                        {this.state.hidden ?  <img src="arrow_up.svg" alt="" /> :  <img src="arrow_down.svg" alt="" />}
+                    </div>
+                    <div className="card" hidden={this.state.hidden}>
+                        <Prices />
+                        <hr className="hr" />
+                        <ClothingTypes />
+                        <hr className="hr" />
+                        <Colors />
+                    </div>
                 </div>
             </div>
         )
